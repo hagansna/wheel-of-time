54e2d1a3c40b4191c74fc9689ee8f5645e244665
Nicholas Hagans <hagansna@gmail.com>
2022-09-19 21:09:50 -0400

    init commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Wheel of Time Info</title>
+        <meta
+          name="description"
+          content="A page with information about The Wheel of Time"
+        />
+      </Head>
+    </div>
+  );
+}
